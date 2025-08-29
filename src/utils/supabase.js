14d4163d71bb9a6b5d26b195f127c0802a5555ec
Abhishek-choudhary1001 <abhishekcool301@gmail.com>
2025-08-29 20:12:02 +0530

    first

--- src/utils/supabase.js
+++ src/utils/supabase.js
@@ -1,6 +1,9 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://srnfisstrzknhlyjnspm.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNybmZpc3N0cnprbmhseWpuc3BtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTIzMTU5MTYsImV4cCI6MjA2Nzg5MTkxNn0.67mkqyjVsNbwkxLyNQ2YoKMnjHjHdDOM_Ou2s8OCP7w'
-const supabase = createClient(supabaseUrl, supabaseKey)
+// Load from Vite env variables
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
+const supabase = createClient(supabaseUrl, supabaseKey);
+
 export default supabase;
